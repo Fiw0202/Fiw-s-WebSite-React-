cf73c7767be0d201b35dbcf076eaaae8ef404074
Fiw0202 <Fiw0202@hotmail.com>
2023-01-30 10:12:56 +0700

    turn on image skill and tools

--- src/component/Skills/Skills.jsx
+++ src/component/Skills/Skills.jsx
@@ -1,14 +1,14 @@
 import "./Skills.css";
-// import Html from '../../../public/Icon/HTML5.png'
-// import Css from '../../../public/Icon/CSS.png'
-// import JavaScript from '../../../public/Icon/Js.png'
-// import NodeJS from '../../../public/Icon/Node.png'
-// import React from '../../../public/Icon/REACT.png'
-// import Express from '../../../public/Icon/Express.png'
-// import MongoDB from '../../../public/Icon/MongoDB.png'
-// import VsCode from '../../../public/Icon/Vscode.png'
-// import Git from '../../../public/Icon/Git.png'
-// import GitHub from '../../../public/Icon/Github.png'
+import Html from '../../../public/Icon/HTML5.png'
+import Css from '../../../public/Icon/CSS.png'
+import JavaScript from '../../../public/Icon/Js.png'
+import NodeJS from '../../../public/Icon/Node.png'
+import React from '../../../public/Icon/REACT.png'
+import Express from '../../../public/Icon/Express.png'
+import MongoDB from '../../../public/Icon/MongoDB.png'
+import VsCode from '../../../public/Icon/Vscode.png'
+import Git from '../../../public/Icon/Git.png'
+import GitHub from '../../../public/Icon/Github.png'
  import Figma from '../../../public/Icon/Figma-logo.svg'
 
 
@@ -19,31 +19,31 @@ function Skills() {
         <h1 id="skillH">Skills</h1>
         <div class="skill-icon">
           <figure>
-            {/*<img src={Html} alt="HTML" />*/}
+            <img src={Html} alt="HTML" />
             <figcaption>HTML5</figcaption>
           </figure>
           <figure>
-            {/*<img src={Css} alt="CSS"/>*/}
+            <img src={Css} alt="CSS"/>
             <figcaption>CSS</figcaption>
           </figure>
           <figure>
-            {/*<img src={JavaScript} />*/}
+            <img src={JavaScript} />
             <figcaption>JavaScript</figcaption>
           </figure>
           <figure>
-            {/*<img src={NodeJS} />*/}
+            <img src={NodeJS} />
             <figcaption>NodeJS</figcaption>
           </figure>
           <figure>
-            {/*<img src={React} />*/}
+            <img src={React} />
             <figcaption>REACT</figcaption>
           </figure>
           <figure>
-            {/*<img src={Express} />*/}
+            <img src={Express} />
             <figcaption>Express</figcaption>
           </figure>
           <figure>
-            {/*<img src={MongoDB} />*/}
+            <img src={MongoDB} />
             <figcaption>MongoDB</figcaption>
           </figure>
         </div>
@@ -52,15 +52,15 @@ function Skills() {
         <h1 id="toolsH">Tools</h1>
         <div class="tools-icon">
           <figure>
-            {/*<img src={VsCode} alt="VsCode" />*/}
+            <img src={VsCode} alt="VsCode" />
             <figcaption>VScode</figcaption>
           </figure>
           <figure>
-            {/*<img src={Git}/>*/}
+            <img src={Git}/>
             <figcaption>Git</figcaption>
           </figure>
           <figure>
-            {/*<img src={GitHub} />*/}
+            <img src={GitHub} />
             <figcaption>Git Hub</figcaption>
           </figure>
           <figure>
